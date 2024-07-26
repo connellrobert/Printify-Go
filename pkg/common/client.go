a61c3613a561aec1ad1fdff423ffd0c817951bcc
connellrobert <robert.lel.connell@gmail.com>
2024-07-25 21:59:04 -0500

    fixing client functions

--- pkg/common/client.go
+++ pkg/common/client.go
@@ -33,6 +33,7 @@ func ListResources[T any](endpoint string) func(c *Client) ([]T, error) {
 			return nil, err
 		}
 
+		req.Header.Set("Content-Type", "application/json")
 		req.Header.Set("Authorization", "Bearer "+c.PAT)
 
 		resp, err := c.Client.Do(req)
@@ -48,224 +49,6 @@ func ListResources[T any](endpoint string) func(c *Client) ([]T, error) {
 		}
 
 		return resources, nil
-	}
-}
-
-func GetResourceById[T any, ID int | string](endpoint string) func(c *Client, id ID) (*T, error) {
-	return func(c *Client, id ID) (*T, error) {
-		req, err := http.NewRequest("GET", c.Host+endpoint, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Add("Authorization", "Bearer "+c.PAT)
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-		var resource T
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-		return &resource, nil
-	}
-}
-
-func PostResourceWithoutReturn[T any](c *Client, endpoint string, body T) error {
-	reqBody, err := json.Marshal(body)
-	if err != nil {
-		return err
-	}
-
-	req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+c.PAT)
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := c.Client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	return nil
-}
-
-func PostResourceWithReturn[T any, R any](endpoint string) func(c *Client, body T) (*R, error) {
-	return func(c *Client, body T) (*R, error) {
-		reqBody, err := json.Marshal(body)
-		if err != nil {
-			return nil, err
-		}
-
-		req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var resource R
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-
-		return &resource, nil
-	}
-}
-
-func PostResourceWithReturnTwoId[T any, R any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
-	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
-		reqBody, err := json.Marshal(body)
-		if err != nil {
-			return nil, err
-		}
-
-		req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var resource R
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-
-		return &resource, nil
-	}
-}
-
-func PostResourceWithoutReturnTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) error {
-	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) error {
-		reqBody, err := json.Marshal(body)
-		if err != nil {
-			return err
-		}
-
-		req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
-		if err != nil {
-			return err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-
-		return nil
-	}
-}
-
-func PostNoResourceWithReturnTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
-	return func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
-		req, err := http.NewRequest("POST", c.Host+endpoint, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var resource T
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-		return &resource, nil
-	}
-}
-
-func PutResourceWithReturnAndTwoId[T any, R any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
-	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
-		reqBody, err := json.Marshal(body)
-		if err != nil {
-			return nil, err
-		}
-
-		req, err := http.NewRequest("PUT", c.Host+endpoint, bytes.NewBuffer(reqBody))
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var resource R
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-
-		return &resource, nil
-	}
-}
-
-func PostResourceWithReturnAndId[T any, R any, ID int | string](endpoint string) func(c *Client, id ID, body T) (*R, error) {
-	return func(c *Client, id ID, body T) (*R, error) {
-		reqBody, err := json.Marshal(body)
-		if err != nil {
-			return nil, err
-		}
-		url := fmt.Sprintf(c.Host+endpoint, id)
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqBody))
-		if err != nil {
-			return nil, err
-		}
-
-		req.Header.Set("Authorization", "Bearer "+c.PAT)
-		req.Header.Set("Content-Type", "application/json")
-
-		resp, err := c.Client.Do(req)
-		if err != nil {
-			return nil, err
-		}
-		defer resp.Body.Close()
-
-		var resource R
-		err = json.NewDecoder(resp.Body).Decode(&resource)
-		if err != nil {
-			return nil, err
-		}
-
-		return &resource, nil
 	}
 }
 
@@ -315,6 +98,29 @@ func ListResourceWithTwoID[T any, IDONE, IDTWO int | string](endpoint string) fu
 		return resources, nil
 	}
 }
+
+func GetResourceById[T any, ID int | string](endpoint string) func(c *Client, id ID) (*T, error) {
+	return func(c *Client, id ID) (*T, error) {
+		req, err := http.NewRequest("GET", fmt.Sprintf(c.Host+endpoint, id), nil)
+		if err != nil {
+			return nil, err
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		var resource T
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+		return &resource, nil
+	}
+}
+
 func GetResourceWithTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
 	return func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
 		url := fmt.Sprintf(c.Host+endpoint, idOne, idTwo)
@@ -338,38 +144,170 @@ func GetResourceWithTwoId[T any, IDONE, IDTWO int | string](endpoint string) fun
 	}
 }
 
-func DeleteResourceWithTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO) error {
-	return func(c *Client, idOne IDONE, idTwo IDTWO) error {
-		url := fmt.Sprintf(c.Host+endpoint, idOne, idTwo)
-		req, err := http.NewRequest("DELETE", url, nil)
+func PostResourceWithoutReturn[T any](endpoint string) func(c *Client, body T) error {
+	return func(c *Client, body T) error {
+		reqBody, err := json.Marshal(body)
 		if err != nil {
 			return err
 		}
 
-		req.Header.Add("Authorization", "Bearer "+c.PAT)
+		req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
 		resp, err := c.Client.Do(req)
 		if err != nil {
 			return err
 		}
 		defer resp.Body.Close()
+
 		return nil
 	}
 }
 
-func DeleteResourceWithId[T any, ID int | string](endpoint string) func(c *Client, id ID) error {
-	return func(c *Client, id ID) error {
-		url := fmt.Sprintf(c.Host+endpoint, id)
-		req, err := http.NewRequest("DELETE", url, nil)
+func PostResourceWithReturn[T any, R any](endpoint string) func(c *Client, body T) (*R, error) {
+	return func(c *Client, body T) (*R, error) {
+		reqBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+
+		req, err := http.NewRequest("POST", c.Host+endpoint, bytes.NewBuffer(reqBody))
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var resource R
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+
+		return &resource, nil
+	}
+}
+
+func PostResourceWithReturnTwoId[T any, R any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
+	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
+		reqBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+
+		req, err := http.NewRequest("POST", fmt.Sprintf(c.Host+endpoint, idOne, idTwo), bytes.NewBuffer(reqBody))
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var resource R
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+
+		return &resource, nil
+	}
+}
+
+func PostResourceWithoutReturnTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) error {
+	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) error {
+		reqBody, err := json.Marshal(body)
 		if err != nil {
 			return err
 		}
-		req.Header.Add("Authorization", "Bearer "+c.PAT)
+
+		req, err := http.NewRequest("POST", fmt.Sprintf(c.Host+endpoint, idOne, idTwo), bytes.NewBuffer(reqBody))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
 		resp, err := c.Client.Do(req)
 		if err != nil {
 			return err
 		}
 		defer resp.Body.Close()
+
 		return nil
+	}
+}
+
+func PostNoResourceWithReturnTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
+	return func(c *Client, idOne IDONE, idTwo IDTWO) (*T, error) {
+		req, err := http.NewRequest("POST", fmt.Sprintf(c.Host+endpoint, idOne, idTwo), nil)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var resource T
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+		return &resource, nil
+	}
+}
+
+func PostResourceWithReturnAndId[T any, R any, ID int | string](endpoint string) func(c *Client, id ID, body T) (*R, error) {
+	return func(c *Client, id ID, body T) (*R, error) {
+		reqBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+		req, err := http.NewRequest("POST", fmt.Sprintf(c.Host+endpoint, id), bytes.NewBuffer(reqBody))
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var resource R
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+
+		return &resource, nil
 	}
 }
 
@@ -399,6 +337,72 @@ func PostNoResourceWithoutReturn[ID int | string](endpoint string) func(c *Clien
 			return err
 		}
 
+		req.Header.Add("Authorization", "Bearer "+c.PAT)
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
+	}
+}
+
+func PutResourceWithReturnAndTwoId[T any, R any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
+	return func(c *Client, idOne IDONE, idTwo IDTWO, body T) (*R, error) {
+		reqBody, err := json.Marshal(body)
+		if err != nil {
+			return nil, err
+		}
+
+		req, err := http.NewRequest("PUT", fmt.Sprintf(c.Host+endpoint, idOne, idTwo), bytes.NewBuffer(reqBody))
+		if err != nil {
+			return nil, err
+		}
+
+		req.Header.Set("Authorization", "Bearer "+c.PAT)
+		req.Header.Set("Content-Type", "application/json")
+
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		var resource R
+		err = json.NewDecoder(resp.Body).Decode(&resource)
+		if err != nil {
+			return nil, err
+		}
+
+		return &resource, nil
+	}
+}
+
+func DeleteResourceWithTwoId[T any, IDONE, IDTWO int | string](endpoint string) func(c *Client, idOne IDONE, idTwo IDTWO) error {
+	return func(c *Client, idOne IDONE, idTwo IDTWO) error {
+		url := fmt.Sprintf(c.Host+endpoint, idOne, idTwo)
+		req, err := http.NewRequest("DELETE", url, nil)
+		if err != nil {
+			return err
+		}
+
+		req.Header.Add("Authorization", "Bearer "+c.PAT)
+		resp, err := c.Client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		return nil
+	}
+}
+
+func DeleteResourceWithId[T any, ID int | string](endpoint string) func(c *Client, id ID) error {
+	return func(c *Client, id ID) error {
+		url := fmt.Sprintf(c.Host+endpoint, id)
+		req, err := http.NewRequest("DELETE", url, nil)
+		if err != nil {
+			return err
+		}
 		req.Header.Add("Authorization", "Bearer "+c.PAT)
 		resp, err := c.Client.Do(req)
 		if err != nil {
